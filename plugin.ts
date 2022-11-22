83961b25842648a95baaf24c0b9b1a6f2626d51c
Himyu <max_b98@hotmail.de>
2022-11-22 14:55:08 +0100

    switch typescript and add vmix config

--- plugin.ts
+++ plugin.ts
@@ -1,31 +1,32 @@
-const { readFileSync } = require('fs')
-const { writeFile } = require('fs/promises')
-const { join } = require('path')
+import type { PluginContext } from '@rcv-prod-toolkit/types'
+import { readFileSync } from 'fs'
+import { writeFile } from 'fs/promises'
+import { join } from 'path'
 
-module.exports = (ctx) => {
-  const configLocation = process.env.CONFIG || join(__dirname, 'config.json')
+module.exports = (ctx: PluginContext) => {
+  const configLocation = process.env.CONFIG || join(__dirname, '..', 'config.json')
   const rawConfig = readFileSync(configLocation)
-  const config = JSON.parse(rawConfig)
+  const config = JSON.parse(rawConfig.toString())
 
   const namespace = ctx.plugin.module.getName()
 
   ctx.LPTE.on(namespace, 'request', (e) => {
     ctx.LPTE.emit({
       meta: {
-        type: e.meta.reply,
+        type: e.meta.reply as string,
         namespace: 'reply',
         version: 1
       },
-      config: config[e.meta.sender.name]
+      config: config[e.meta.sender!.name]
     })
   })
 
   ctx.LPTE.on(namespace, 'set', async (e) => {
-    if (config[e.meta.sender.name] === undefined) {
-      config[e.meta.sender.name] = e.config
+    if (config[e.meta.sender!.name] === undefined) {
+      config[e.meta.sender!.name] = e.config
     } else {
       for (const key of Object.keys(e.config)) {
-        config[e.meta.sender.name][key] = e.config[key]
+        config[e.meta.sender!.name][key] = e.config[key]
       }
     }
 
@@ -34,10 +35,10 @@ module.exports = (ctx) => {
         join(__dirname, './config.json'),
         JSON.stringify(config, null, 2)
       )
-      ctx.log.info('config for ' + e.meta.sender.name + ' saved!')
+      ctx.log.info('config for ' + e.meta.sender!.name + ' saved!')
     } catch (error) {
       ctx.log.error(
-        'config for ' + e.meta.sender.name + ` could not be saved! ${error}`
+        'config for ' + e.meta.sender!.name + ` could not be saved! ${error}`
       )
     }
   })
